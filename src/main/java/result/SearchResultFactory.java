b275c8f8d0d004083ad154af9dbe665f4b03d569
Jonghun Park <zorba.heron@gmail.com>
2017-03-09 17:07:42 +0900

    SearchResult type exception handling added

--- src/main/java/result/SearchResultFactory.java
+++ src/main/java/result/SearchResultFactory.java
@@ -3,22 +3,25 @@ package result;
 public class SearchResultFactory {
 
   public static SearchResult create(ResultFormat resultFormat) {
-    
-    if (resultFormat.isSpikeIn()) {
-      return new SpikeInResult();
-    }
-    else if (resultFormat.isMsgf()) {
-      return new MsgfPlusResult();
-    } 
-    else if (resultFormat.isPin()) {
-      return new PinResult();
-    }
-    else {
-      System.err.println("Other format is not avaialble");
+    try {
+      if (resultFormat.isSpikeIn()) {
+        return new SpikeInResult();
+      }
+      else if (resultFormat.isMsgf()) {
+        return new MsgfPlusResult();
+      } 
+      else if (resultFormat.isPin()) {
+        return new PinResult();
+      }
+      else {
+        System.err.println("Other format is not avaialble");
+        System.exit(-1);
+      }
+    } catch (EnumConstantNotPresentException e) {
+      e.printStackTrace();
       System.exit(-1);
     }
-    
-    return null;
+    return null; 
   }
   
 }
